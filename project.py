0cc9a4fc78966743ed97cf7b08e62bd47d1783cc
Jessica <jessica@Jessicas-MacBook-Pro-11.local>
2025-04-08 10:54:10 -0400

    test

--- project.py
+++ project.py
@@ -1,1 +1,8 @@
-#hellp wprjkeng
+import requests
+import json
+
+# get the data
+response = requests.get('https://maps.googleapis.com/maps/api/')
+data = response.text
+in_dict = json.loads(data)
+print(in_dict.get(""))
