d41a295f20ec6213b1ee036e7f77a2a767908918
田辺莉麻 <riotanabe@tanaberiasas-MacBook-Pro.local>
2025-04-08 11:09:14 -0400

    first change

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -7,10 +7,10 @@ conn = sqlite3.connect("travel_weather.db")
 
 # Query average travel time by location
 df_travel = pd.read_sql_query("""
-    SELECT locations.latitude, locations.longitude, AVG(travel_data.travel_time) AS avg_travel_time 
-    FROM travel_data 
-    JOIN locations ON travel_data.location_id = locations.id 
-    GROUP BY locations.id
+SELECT locations.latitude, locations.longitude, AVG(travel_data.travel_time) AS avg_travel_time 
+FROM travel_data 
+JOIN locations ON travel_data.location_id = locations.id 
+GROUP BY locations.id
 """, conn)
 
 plt.figure(figsize=(8, 6))
@@ -22,10 +22,10 @@ plt.show()
 
 # Query and plot average temperature
 df_weather = pd.read_sql_query("""
-    SELECT locations.latitude, locations.longitude, AVG(weather_data.temperature) AS avg_temperature 
-    FROM weather_data 
-    JOIN locations ON weather_data.location_id = locations.id 
-    GROUP BY locations.id
+SELECT locations.latitude, locations.longitude, AVG(weather_data.temperature) AS avg_temperature 
+FROM weather_data 
+JOIN locations ON weather_data.location_id = locations.id 
+GROUP BY locations.id
 """, conn)
 
 plt.figure(figsize=(8, 6))
